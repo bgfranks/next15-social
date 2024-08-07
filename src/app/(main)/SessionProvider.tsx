5f15c8f99502f3955e841ba595baf1e30b5b6c5f
Brandon Franks <bgfranks@Brandons-Air.attlocal.net>
2024-08-07 19:58:54 -0400

    set up a usesession custom hook to check if the session is null

--- src/app/(main)/SessionProvider.tsx
+++ src/app/(main)/SessionProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Session, User } from "lucia";
-import React, { createContext } from "react";
+import React, { createContext, useContext } from "react";
 
 interface SessionContext {
   user: User;
@@ -17,4 +17,14 @@ export default function SessionProvider({
   return (
     <SessionContext.Provider value={value}>{children}</SessionContext.Provider>
   );
+}
+
+export function useSession() {
+  const context = useContext(SessionContext);
+
+  if (!context) {
+    throw new Error("useSession must be used within a session provider");
+  }
+
+  return context;
 }
