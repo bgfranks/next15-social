27670380764fd985d9c7fcd8beba9057a144e983
Brandon Franks <bgfranks@Brandons-Air.attlocal.net>
2024-08-07 19:56:23 -0400

    set up the session provider and the main layout to check if there is a session

---
+++ src/app/(main)/SessionProvider.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Session, User } from "lucia";
+import React, { createContext } from "react";
+
+interface SessionContext {
+  user: User;
+  session: Session;
+}
+
+const SessionContext = createContext<SessionContext | null>(null);
+
+export default function SessionProvider({
+  children,
+  value,
+}: React.PropsWithChildren<{ value: SessionContext }>) {
+  return (
+    <SessionContext.Provider value={value}>{children}</SessionContext.Provider>
+  );
+}
