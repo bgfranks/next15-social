90d791f5e8d0aead15c1f432e12a6238c8260471
Brandon Franks <bgfranks@Brandons-Air.attlocal.net>
2024-08-19 12:04:51 -0400

    created the UI for the post settings and delete the post

---
+++ src/components/posts/PostSettingsButton.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { PostData } from "@/lib/types";
+import { useState } from "react";
+import DeletePostModal from "./DeletePostModal";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { MoreHorizontal, Trash2 } from "lucide-react";
+
+interface PostSettingsButtonProps {
+  post: PostData;
+  className?: string;
+}
+
+export default function PostSettingsButton({
+  post,
+  className,
+}: PostSettingsButtonProps) {
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button size="icon" variant="ghost" className={className}>
+            <MoreHorizontal className="size-5 text-muted-foreground" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem onClick={() => setShowDeleteDialog(true)}>
+            <span className="flex items-center gap-3 text-destructive">
+              <Trash2 className="size-4" />
+              Delete
+            </span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DeletePostModal
+        post={post}
+        open={showDeleteDialog}
+        onClose={() => setShowDeleteDialog(false)}
+      />
+    </>
+  );
+}
